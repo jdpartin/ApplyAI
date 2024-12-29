910d680b6ef8f3e51b6bcd0cfd35acde0be27244
Jacob Partin <jdpartin@icloud.com>
2024-12-28 23:59:30 -0600

    Update paths to not use file extensions

--- app/myapp/views.py
+++ app/myapp/views.py
@@ -6,8 +6,10 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'frontend/index.html')
 
+# path in urls is sign-in
 def sign_in(request):
     return render(request, 'frontend/sign-in.html')
 
+# path in urls is sign-up
 def sign_up(request):
     return render(request, 'frontend/sign-up.html')
